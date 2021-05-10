ab29e921b8dcf0a912b89c4435b140b5c0aacccc
ssuyash <kumarsuyash94@gmail.com>
2021-05-10 21:35:56 +0530

    js

---
+++ js/script.js
@@ -0,0 +1,55 @@
+
+//numbers
+var num1  = 3
+var num2  = 3.5
+var num3  = 0b1100
+var num4  = 0O6565
+var num5  = 0Xfff
+var num6  = Infinity
+var num7  = NaN
+
+//boolean values 
+var isClassRunning = true
+var isTimeOut = false
+
+//string values  
+var info = 'my name is suyash'
+var profession = "Full Stack Developer"
+var hobbies = `Programming`
+
+
+//undefined values  
+var b = undefined 
+
+
+// Array values 
+var arr = ["suyash", 'kumar', true, 26]
+arr[1]
+
+
+//Object values 
+var person = {
+    name:"suyahs",
+    age:26,
+    languageKnown:["Python", "JS"]
+} 
+//person['age']
+//person.age
+
+
+var emptyObj = null 
+
+
+
+
+"suyash"
+
+
+veg  => 
+milk => 
+
+
+// Strictly Typed => TypeScript
+// Loosely typed
+
+//number 
