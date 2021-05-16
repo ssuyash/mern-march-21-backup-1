93b65d73f7e199b7bf1b7eeadce84779dfa03b00
ssuyash <kumarsuyash94@gmail.com>
2021-05-16 09:22:33 +0530

    loop

---
+++ js/loop.js
@@ -0,0 +1,30 @@
+//for   => intial poing and end point know
+//while => conditional
+
+
+// for(var i=1; i<=3; i++){
+//     //line of code
+//     console.log(i)
+// }
+
+
+// for(var i=0; i<=3; i++){
+//    for(var j=0; j<= 2; j++){
+//        console.log(i, j)
+//    }
+// }
+
+
+for(var i=1; i<=3; i++){
+    for(var j=i; j>=0; j--){
+        console.log(i, j)
+    }
+}
+
+
+
+
+
+//undefined
+// 11 => 2
+//10 =>2
--- js/problems/ibus-problem.js
+++ js/problems/ibus-problem.js
@@ -10,11 +10,13 @@ var stations = {
     NVLK:{dist:18, nextStation:'', prevStation:"BHWN"}
 }
 
+station['MHOW']['dist']
+
 
 //stations = ['MHOW', 'RAU', 'RJGS', 'BHWN', 'NV']
 
-var src = "NVLK"
-var dst = "BHWN"
+var src = "RAU"
+var dst = "NVLK"
 
 var srcDst = stations[src]['dist']
 var dstDst = stations[dst]['dist']
@@ -22,17 +24,24 @@ var srcNextStation = stations[src]['nextStation']
 var srcPrevStation = stations[src]['prevStation']
 var isCont = false
 
+var fare = 5
+
 if(dst == srcNextStation || dst==srcPrevStation){
     isCont = true
+    fare = 5
 }
 
-var fare = 5
 
 if(!isCont){
     var travelDst = dstDst-srcDst 
+    travelDst =  travelDst < 0 ? -1*travelDst  : travelDst
     if(travelDst > 3){
         fare += (travelDst-3)*3
     }
+}
+
+if(src == dst){
+    fare = 0
 }
 
 console.log(fare)
