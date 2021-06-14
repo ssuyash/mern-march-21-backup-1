4d664edb2304f8784da29f48bb24bfea5e986845
ssuyash <kumarsuyash94@gmail.com>
2021-06-14 22:03:17 +0530

    inheritance

--- js/oops/class.js
+++ js/oops/class.js
@@ -1,6 +1,7 @@
 class Student {
 
     constructor(name, course){
+        console.log("I am constructor of student class")
         this.name = name;
         this.course = course;
     }
@@ -12,12 +13,38 @@ class Student {
     
 }
 
-var s1 =  new Student("karan", 'mern')
-s1.getDetails()
+// var s1 =  new Student("karan", 'mern')
+// s1.getDetails()
 
 
-var s2 =  new Student("Harshit", 'mern')
-s2.getDetails()
-//console.log(s1)
+// var s2 =  new Student("Harshit", 'mern')
+// s2.getDetails()
+// //console.log(s1)
 
 
+class Library extends Student{
+    constructor(name, course, membership){
+        super()
+        console.log("I am constructor of Library class")
+        this.membership = membership
+    }
+
+    getMemberShip(){
+        return `${this.name} has ${this.membership} membership available`
+    }
+
+    //overriding
+    getDetails = ()=>{
+        super.getDetails()
+    }
+}
+
+
+var lib1 = new Library("karan", 'mern', 'lifetime')
+console.log(lib1.getMemberShip())
+
+var lib2 = new Library("Harshi", 'mern', '35 years')
+console.log(lib2.getMemberShip())
+
+
+var student = new Student()
--- js/oops/consfn.js
+++ js/oops/consfn.js
@@ -14,3 +14,28 @@ var Student = function(name, course){
 var s1 = new Student("suyash", 'mern')
 
 console.log(s1)
+
+
+//call apply bind
+//map reduce filter
+
+Array.prototype.sum = function(){
+    return this.reduce((acc, el)=>acc+el, 0)
+}
+
+
+
+
+var arr = [1, 2, 3]
+console.log(arr.sum())
+console.log(arr)
+
+
+var arr2 = []
+console.log(arr2.sum())
+
+
+
+
+//overriding    =>  
+//overloading   => 
